eae099ef0de12ccac2eea9ac7a288f5a7592b4e3
Alexandra2899 <59295650+Alexandra2899@users.noreply.github.com>
2020-03-11 15:46:02 +0000

    Add files via upload

---
+++ Cliente.h
@@ -0,0 +1,22 @@
+#ifndef cliente_h
+#define cliente_h
+
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct AVL *AVL;
+typedef AVL *Cliente;
+
+Cliente initCliente();
+void destroyCliente(Cliente c);
+void readFiletoCliente(Cliente c, FILE* f);
+int altura(AVL a);
+int max(int a,int b);
+AVL rotateR(AVL a);
+AVL rotateL(AVL a);
+AVL insertAVL(AVL a,char* x);
+void printAVL(AVL a, int i);
+
+#endif cliente_h
