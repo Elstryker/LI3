18cb52b917671cd128a1eac20d743ff36d284dd2
Alexandra2899 <59295650+Alexandra2899@users.noreply.github.com>
2020-03-11 15:44:15 +0000

    Add files via upload

---
+++ Produto.h
@@ -0,0 +1,23 @@
+#ifndef produto_h
+#define produto_h
+
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct AVL *AVL;
+typedef AVL *Produto;
+
+Produto initProduto();
+void destroyProduto(Produto p);
+void readFiletoProduto(Produto p, FILE* f);
+int compareKey(char* s1, char* s2);
+int altura(AVL a);
+int max(int a,int b);
+AVL rotateR(AVL a);
+AVL rotateL(AVL a);
+AVL insertAVL(AVL a,char* x);
+void printAVL(AVL a, int i);
+
+#endif produto_h
