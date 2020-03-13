608eec7fe65fae28d2e8c5e6b160608af9a4d13f
Elstryker <rubencorreiacerqueira@gmail.com>
2020-03-13 16:24:32 +0000

    Ya tratem disso

--- main.c
+++ main.c
@@ -7,11 +7,37 @@
 //
 #include "interface.h"
 
+int percorreAVLP(AVLP a) {
+    int i=0;
+    if(a) {
+        i+=percorreAVLP(a->left);
+        i++;
+        i+=percorreAVLP(a->right);
+    }
+    return i;
+}
+
+int percorreAVLC(AVLC a) {
+    int i=0;
+    if(a) {
+        i+=percorreAVLC(a->left);
+        i+=percorreAVLP(a->prod);
+        i+=percorreAVLC(a->right);
+    }
+    return i;
+}
+
 int main(int argc, const char * argv[]) {
+    int i,j,r=0;
     SGV a;
     a=initSGV();
     a=loadSGVFromFiles(a);
     printf("\n\nDone!\n\n");
-    //getProductsStartedByLetter(a,'B');
+    for(i=0;i<3;i++) {
+        for(j=0;j<12;j++) {
+            r+=percorreAVLC(a.v[i][j]);
+        }
+    }
+    printf("%d\n\n",r);
     return 0;
 }
